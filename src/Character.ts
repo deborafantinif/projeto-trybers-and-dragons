7df03f28efcc2f4405f22269edd30815cd589b71
deborafantinif <deboraferfan@gmail.com>
2022-08-10 16:04:33 -0300

    feat[req 08]: interface SimpleFighter

--- src/Character.ts
+++ src/Character.ts
@@ -1,7 +1,8 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private _race: Race;
@@ -13,23 +14,19 @@ export default class Character implements Fighter {
   private _dexterity: number;
   private _energy: Energy;
 
-  static randomNumber(minNumber: number, maxNumber: number): number {
-    return Math.floor(Math.random() * maxNumber + minNumber);
-  }
-
   constructor(
     private _name: string,
   ) {
-    this._dexterity = Character.randomNumber(0, 11);
+    this._dexterity = getRandomInt(0, 10);
     this._race = new Elf(this._name, this._dexterity);
     this._archetype = new Mage(this._name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
-    this._strength = Character.randomNumber(1, 10);
-    this._defense = Character.randomNumber(1, 10);
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
     this._energy = {
       type_: this._archetype.energyType,
-      amount: Character.randomNumber(1, 10),
+      amount: getRandomInt(1, 10),
     };
   }
 
@@ -74,15 +71,15 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
   levelUp(): void {
-    this._maxLifePoints += Character.randomNumber(1, 10);
-    this._strength += Character.randomNumber(1, 10);
-    this._dexterity += Character.randomNumber(1, 10);
-    this._defense += Character.randomNumber(1, 10);
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
     this._energy.amount = 10;
 
     if (this._maxLifePoints > this._race.maxLifePoints) {
